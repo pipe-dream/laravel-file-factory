807d194268eba67bf2f3fda20d5c4abefb7c9c51
Anders Jurisoo <jurisoo@hotmail.com>
2019-06-14 15:41:40 +0200

    Removed console statement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ require('./bootstrap/assignConstants')
 
 // this way we can use the templates in pipes or where ever necessary
 let templates = require('./templates.js')
-console.log(templates.stubs)
 
 exports.text = "someText"
